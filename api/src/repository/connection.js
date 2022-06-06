fdeb4de1e767a7bf92394204e3e97871f8f9f2aa
Robert dos Santos Silva <Robert dos Santos Silva>
2022-06-06 08:49:57 -0300

    first commit

---
+++ api/src/repository/connection.js
@@ -0,0 +1,9 @@
+import mysql from 'mysql2/promise'
+const con=await mysql.createConnection({
+    host:process .env.mysql_host,
+    user:process.env.mysql_user,
+    password:process.env.mysql_pwd,
+    database:process.env.mysql_db,
+})console.log ('BD conectado');
+
+export { con }
